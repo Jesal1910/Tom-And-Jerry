c5ab5ccbf3b54071e7424f43466a16177362239c
Jesal1910 <77778144+Jesal1910@users.noreply.github.com>
2021-07-18 13:29:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 var bgImg;
 var cat,catImg1,catImg2,catImg3;
-var mouse,mouseImg1,mouseImg2;
+var mouse,mouseImg1,mouseImg2,mouseImg3;
 function preload() {
     //load the images here
-    bgImg=loadImage("garden.png")
-    catImg1=loadImage("cat1.png")
-    catImg2=loadAnimation("cat2.png")
-    mouseImg1=loadImage("mouse1.png")
-    mouseImg2=loadAnimation("mouse2.png")
- 
+    bgImg=loadImage("images/garden.png")
+    catImg1=loadImage("images/cat1.png")
+    catImg2=loadAnimation("images/cat2.png","images/cat3.png")
+    catImg3=loadImage("images/cat4.png")
+    mouseImg1=loadImage("images/mouse1.png")
+    mouseImg2=loadAnimation("images/mouse2.png","images/mouse3.png")
+    mouseImg3=loadImage("images/mouse4.png")
+
 }
 
 function setup(){
@@ -27,9 +29,14 @@ function draw() {
     background(bgImg);
     //Write condition here to evalute if tom and jerry collide
   if (cat.x - mouse.x < (cat.width - mouse.width)/2) {
-      cat.addAnimation("catRunning",catImg2);
-      cat.changeAnimation("catRunning");
       cat.velocityX=0
+      cat.addAnimation("catRunning",catImg3);
+      cat.x = 300;
+      cat.scale=0.2;
+      cat.changeAnimation("catRunning");
+      mouse.addAnimation("mouseRunning",mouseImg3);
+      mouse.scale=0.2;
+      mouse.changeAnimation("mouseRunning")
   }
     drawSprites();
 }
@@ -40,12 +47,18 @@ function keyPressed(){
   //For moving and changing animation write code here
  if (keyCode === LEFT_ARROW) {
      cat.velocityX=-2;
+     cat.addAnimation("catRunning",catImg2);
+     cat.changeAnimation("catRunning");
+
      mouse.addAnimation("mouseTeasing",mouseImg2);
      mouse.changeAnimation("mouseTeasing");
      mouse.frameDelay=25;
  }
  if (keyCode === RIGHT_ARROW) {
      cat.velocityX=2;
+     cat.addAnimation("catRunning",catImg2);
+     cat.changeAnimation("catRunning");
+     
     mouse.addAnimation("mouseTeasing",mouseImg2);
     mouse.changeAnimation("mouseTeasing");
     mouse.frameDelay=25;
